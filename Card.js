d5a3cf8a1691258db4e6d2514832156380dd7745
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Air-2.local>
2019-02-12 14:19:14 -0500

    react with css

--- Card.js
+++ Card.js
@@ -2,25 +2,25 @@ function Card(props){
     console.log(props)
 const name = "Cody Summers";
 // const title = "React form the start"
-const title = props.title;
+const course = props.data.course;
+const instructor = props.data.instructor;
+const image = props.data.image;
 const saleOn = false;
    return (
-    <div className="row">
     <div className="col s2">
     <div className="card hoverable small">
         <div className="card-image">
-            <img src="http://lorempixel.com/400/400/nature/" />
+            <img src={image} />
         </div>
         <div className="card-content">
-            <p>{title}</p>
-            <p>{name}</p>
+            <p>{course}</p>
+            <p>{instructor}</p>
         </div>
         <div className="card-action">
             <a href="#">${saleOn ? 9.99 : 59.99}</a>
         </div>
     </div>
 </div>    
-</div>
    ) 
     
 }
